8994a2c8590aa2bbe9f419f76edd52ee23bce66d
katykaze <kathezambrano9@gmail.com>
2023-04-24 19:29:36 +0200

    cambio en css estilos, y boton logout

--- src/views/MainView.vue
+++ src/views/MainView.vue
@@ -1,43 +1,44 @@
 <template>
-    <LMain>
-        <template #header>
-            <h1> NEW VIEW 🏡🏠</h1>
-        </template>
-        <template #main>
-            <CButton/>
-            <p>HELLOW WOLRD 🌎 </p>
-            <p>This is a new view</p>
-        </template>
-        <template #footer>
-            <p>📲 social Media</p>
-        </template>
-    </LMain>
-
+  <LMain>
+    <template #header>
+      <h1>NEW VIEW 🏡🏠</h1>
+    </template>
+    <template #main>
+      <p>HELLOW WOLRD 🌎</p>
+      <p>This is a new view</p>
+      <CButton :onclick="doLogout">Log Out</CButton>
+    </template>
+    <template #footer>
+      <p>📲 social Media</p>
+    </template>
+  </LMain>
 </template>
 <script>
 import LMain from '../layouts/l-main.vue'
 import CButton from '../components/c-button.vue'
 import { userStore } from '../stores/user'
 
-export default{
-    name: 'MainView',
-    data(){
-        return {
-            isPending:true
-        }
-    },
-    components:{
-        LMain,
-        CButton
-    },
-    methods:{
-        doLogout(){
-            
-        },
-        
+export default {
+  name: 'MainView',
+  data() {
+    return {
+      isSending: false
+      //   isPending: true
     }
+  },
+  components: {
+    LMain,
+    CButton
+  },
+  methods: {
+    doLogout() {
+      this.isSending = true
+      const useUserStore = userStore()
+      useUserStore.logout()
+      this.$router.push({ name: 'login' })
+    }
+  }
 }
 </script>
 
-<style lang="scss"> 
-</style>
+<style lang="scss"></style>
